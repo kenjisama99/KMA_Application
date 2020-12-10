0a3c80327794cd60eb8c4b84f47f7634e448411c
NHD <duongnguyen213119@gmail.com>
2020-12-10 22:26:02 +0700

    duong

--- app/src/main/java/com/example/kma_application/contact.java
+++ app/src/main/java/com/example/kma_application/contact.java
@@ -17,14 +17,12 @@ public class contact extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact);
 
-
-        private void anhxa() {
-            lvChat = (ListView)findViewById(R.id.listviewChat);
-            editText = (EditText)findViewById(R.id.editextContent);
-            btnCall = (ImageButton)findViewById(R.id.buttonCall);
-            btnSend = (ImageButton)findViewById(R.id.buttonSend);
-        }
-
+    }
+    private void anhxa() {
+        lvChat = (ListView)findViewById(R.id.listviewChat);
+        editText = (EditText)findViewById(R.id.editextContent);
+        btnCall = (ImageButton)findViewById(R.id.buttonCall);
+        btnSend = (ImageButton)findViewById(R.id.buttonSend);
     }
 
 
