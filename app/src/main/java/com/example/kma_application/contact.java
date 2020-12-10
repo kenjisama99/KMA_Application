ffcf72f09367061ec44fa4a0dc5ac7bb0c2fe639
NHD <duongnguyen213119@gmail.com>
2020-12-10 21:57:32 +0700

    sua file contact.xml

--- app/src/main/java/com/example/kma_application/contact.java
+++ app/src/main/java/com/example/kma_application/contact.java
@@ -3,12 +3,29 @@ package com.example.kma_application;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ListView;
 
 public class contact extends AppCompatActivity {
+    ListView lvChat;
+    EditText editText;
+    ImageButton btnCall, btnSend;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact);
+
+
+        private void anhxa() {
+            lvChat = (ListView)findViewById(R.id.listviewChat);
+            editText = (EditText)findViewById(R.id.editextContent);
+            btnCall = (ImageButton)findViewById(R.id.buttonCall);
+            btnSend = (ImageButton)findViewById(R.id.buttonSend);
+        }
+
     }
+
+
 }
