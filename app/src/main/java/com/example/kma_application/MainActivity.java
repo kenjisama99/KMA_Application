275b0487cb0fbedcd8cbc2e3043c761f46dd76b8
kenjisama99 <galaxysupreme9@gmail.com>
2020-12-25 11:17:03 +0700

    launcher home

--- app/src/main/java/com/example/kma_application/MainActivity.java
+++ app/src/main/java/com/example/kma_application/MainActivity.java
@@ -3,7 +3,10 @@ package com.example.kma_application;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.transition.Transition;
@@ -45,16 +48,19 @@ public class MainActivity extends AppCompatActivity implements LoadInfosTask.Asy
 
         loadInfosTask.execute();
 
-        btInfo.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                onClickBtInfo();
-
-            }
-        });
+//        btInfo.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                onClickBtInfo();
+//
+//            }
+//        });
 
         BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
         bottomNav.setOnNavigationItemReselectedListener(navListener);
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction ft_add = fm.beginTransaction();
+        ft_add.add(R.id.framelayout_container, new homeFragment()).commit();
     }
 
 
