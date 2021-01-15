c84be029a48e243fa03a82c62586ec82b152efce
kenjisama99 <galaxysupreme9@gmail.com>
2021-01-15 22:24:27 +0700

    acept admin

--- app/src/main/java/com/example/kma_application/Activity/AdminHomeActivity.java
+++ app/src/main/java/com/example/kma_application/Activity/AdminHomeActivity.java
@@ -13,14 +13,14 @@ import android.widget.Toast;
 import com.example.kma_application.R;
 
 public class AdminHomeActivity extends AppCompatActivity {
-
+    Button showMenu;
     private String textSelected;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_home);
 
-        Button showMenu = (Button) findViewById(R.id.show_dropdown_menu);
+         showMenu = (Button) findViewById(R.id.show_dropdown_menu);
         showMenu.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -85,19 +85,45 @@ public class AdminHomeActivity extends AppCompatActivity {
     }
 
     private void onClickManageHealth() {
-
+        if(!showMenu.getText().equals("Chọn")){
+            Intent manageNotification = new Intent(this, TeacherHealthActivity.class);
+            manageNotification.putExtra("class",showMenu.getText().toString().trim());
+            startActivity(manageNotification);
+        }
+        else
+            Toast.makeText(this,"Yêu cần bạn chọn lớp",Toast.LENGTH_LONG).show();
     }
 
     private void onClickManageAbsent() {
+        if(!showMenu.getText().equals("Chọn")){
+            Intent manageNotification = new Intent(this, TeacherAbsentActivity.class);
+            manageNotification.putExtra("class",showMenu.getText().toString().trim());
+            startActivity(manageNotification);
+        }
+        else
+            Toast.makeText(this,"Yêu cần bạn chọn lớp",Toast.LENGTH_LONG).show();
 
     }
 
     private void onClickManageMedicine() {
-
+        if(!showMenu.getText().equals("Chọn")){
+            Intent manageNotification = new Intent(this, TeacherPrescriptionActivity.class);
+            manageNotification.putExtra("class",showMenu.getText().toString().trim());
+            startActivity(manageNotification);
+        }
+        else
+            Toast.makeText(this,"Yêu cần bạn chọn lớp",Toast.LENGTH_LONG).show();
     }
 
     private void onClickManageGallery() {
-
+        if(!showMenu.getText().equals("Chọn")){
+            Intent manageNotification = new Intent(this, GalleryActivity.class);
+            manageNotification.putExtra("class",showMenu.getText().toString().trim());
+            manageNotification.putExtra("role","teacher");
+            startActivity(manageNotification);
+        }
+        else
+            Toast.makeText(this,"Yêu cần bạn chọn lớp",Toast.LENGTH_LONG).show();
     }
 
     private void onClickManageNotification() {
