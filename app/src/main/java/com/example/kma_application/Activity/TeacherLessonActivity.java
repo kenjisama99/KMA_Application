c5741760fd5d6d3bb3e3a48e7e21e638ac925b27
khoai000 <duongnguyen213119@gmail.com>
2021-09-15 18:33:57 +0700

    add file

--- app/src/main/java/com/example/kma_application/Activity/TeacherLessonActivity.java
+++ app/src/main/java/com/example/kma_application/Activity/TeacherLessonActivity.java
@@ -2,7 +2,10 @@ package com.example.kma_application.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.kma_application.R;
 
@@ -12,5 +15,19 @@ public class TeacherLessonActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate( savedInstanceState );
         setContentView( R.layout.activity_teacher_lesson );
+
+        Button btCreateLesson = (Button)findViewById(R.id.buttonCreateLesson);
+        btCreateLesson.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onClickCreateLesson();
+            }
+        });
+    }
+
+    private void onClickCreateLesson() {
+        Intent createLesson = new Intent(this, AdminCreateNotificationActivity.class);
+//        createNotification.putExtra("role","admin");
+        startActivity(createLesson);
     }
 }
