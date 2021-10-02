58a28a87dc28fe5862784faf37a2e6ec7e8df3e5
khoai000 <duongnguyen213119@gmail.com>
2021-10-02 23:09:55 +0700

    change

--- app/src/main/java/com/example/kma_application/Activity/TeacherPostLessonActivity.java
+++ app/src/main/java/com/example/kma_application/Activity/TeacherPostLessonActivity.java
@@ -14,41 +14,7 @@ public class TeacherPostLessonActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate( savedInstanceState );
-        setContentView( R.layout.activity_teacher_post_lesson );
-
-        ImageButton btBackLesson = (ImageButton) findViewById(R.id.btBackLesson);
-        Button btPostLesson = (Button)findViewById(R.id.btPostLesson);
-        ImageButton btDelLesson = (ImageButton) findViewById( R.id.btDelLesson );
-        ImageButton btAddImgLesson = (ImageButton) findViewById( R.id.btAddImgLesson );
-
-        btBackLesson.setOnClickListener( new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                finish();
-            }
-        } );
-
-        btPostLesson.setOnClickListener( new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                onClickPostlesson();
-            }
-        } );
-
-        btDelLesson.setOnClickListener( new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                onClickDelLesson();
-            }
-        } );
-
-        btAddImgLesson.setOnClickListener( new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                onClicjImgLesson();
-            }
-        } );
-
+        setContentView( R.layout.activity_teacher_post_lesson);
     }
 
     private void onClicjImgLesson() {
