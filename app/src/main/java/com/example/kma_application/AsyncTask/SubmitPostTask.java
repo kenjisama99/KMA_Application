5df36f3a1ea276bde45b5d7bc111469bda6b0754
kenjisama99 <galaxysupreme9@gmail.com>
2021-10-04 20:28:59 +0700

    readd name

--- app/src/main/java/com/example/kma_application/AsyncTask/SubmitPostTask.java
+++ app/src/main/java/com/example/kma_application/AsyncTask/SubmitPostTask.java
@@ -20,12 +20,13 @@ import java.util.Calendar;
 
 public class SubmitPostTask extends AsyncTask<Void,Void,String> {
     private Context context;
-    String _class, description, originalBase64, resizeBase64;
+    String _class, name, description, originalBase64, resizeBase64;
     PostStatusActivity postStatusActivity;
 
-    public SubmitPostTask(Context context, String _class, String description, String originalBase64, String resizeBase64, PostStatusActivity postStatusActivity) {
+    public SubmitPostTask(Context context, String _class, String name, String description, String originalBase64, String resizeBase64, PostStatusActivity postStatusActivity) {
         this.context = context;
         this._class = _class;
+        this.name = name;
         this.description = description;
         this.originalBase64 = originalBase64;
         this.resizeBase64 = resizeBase64;
@@ -58,6 +59,7 @@ public class SubmitPostTask extends AsyncTask<Void,Void,String> {
 
         return "{\"_class\":\"" + _class + "\","
                 +"\"date\":\"" + date + "\","
+                +"\"name\":\"" + name + "\","
                 +"\"description\":\"" + description +"\","
                 + "\"originalBase64\":\"" + originalBase64 + "\","
                 + "\"resizeBase64\":\"" + resizeBase64 + "\"}";
